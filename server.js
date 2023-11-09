47e110f6146af914b0f38ca6d0d37dbe8c2f57c2
현예닮 <87516405+yedamhy@users.noreply.github.com>
2023-11-10 04:24:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', authController.renderHomePage);
 // 서버 시작
 
 
-const PORT = 'staging-dot-even-arc-404608.uk.r.appspot.com' || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`${PORT}번 포트에서 서버 대기 중입니다.`)
     console.log(`Environment variables: ${JSON.stringify(process.env)}`); // 환경 변수 전체를 출력합니다.
