7aae287db18d40886a6efb388e0a04c0c3bc0c5c
Jihye <jihyeeee511@gmail.com>
2023-11-29 14:11:39 +0900

    메인화면 포스트 업로드기능, 포스트 스레드 구현

--- assets/js/posting.js
+++ assets/js/posting.js
@@ -1,72 +1,182 @@
-let modal = document.getElementById('post-modal');
-let commentModal = document.getElementById('comment-modal');
-let addButton = document.getElementById('add-post-button');
-let postsContainer = document.getElementById('posts-container');
-let currentPost = null;
+const { Router } = require("express");
+const router = require("../../login/routes/auth");
+const fs = require("fs");
 
 
-function openPostModal() {
-    modal.style.display = 'block';
-    // 새로운 글을 작성할 때 이전 내용 초기화
-    document.getElementById('post-content').value = '';
-}
+const bodyParser = require('body-parser');
+const dbPool = require('../../mysql/index');
 
-function closePostModal() {
-    modal.style.display = 'none';
-}
 
-function openCommentModal(postElement) {
-    currentPost = postElement;
-    let postContent = postElement.getElementsByClassName('post-content')[0].textContent;
-    document.getElementById('post-content-container').innerHTML = '<strong>게시글:</strong> ' + postContent;
 
-    let commentsContainer = document.getElementById('comments-container');
-    commentsContainer.innerHTML = '';
+router.get("/",function(req,res,next) {
+    fs.readFile("/index.html", (err, data) => {
+        if (err) {
+          res.send("error");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+    });
+    
+// 이거 영주가 처음에 개발했을때 만들어 둔건데 혹시 도움될까봐 지우지않을게! 필요없으면 지워도 됩니당
+// function openCommentModal(postElement) {
+//     currentPost = postElement;
+//     let postContent = postElement.getElementsByClassName('post-content')[0].textContent;
+//     document.getElementById('post-content-container').innerHTML = '<strong>게시글:</strong> ' + postContent;
 
-    let existingComments = postElement.getElementsByClassName('comment');
-    for (let i = 0; i < existingComments.length; i++) {
-        let commentElement = document.createElement('div');
-        commentElement.classList.add('comment');
-        commentElement.textContent = existingComments[i].textContent;
-        commentsContainer.appendChild(commentElement);
+//     let commentsContainer = document.getElementById('comments-container');
+//     commentsContainer.innerHTML = '';
+
+//     let existingComments = postElement.getElementsByClassName('comment');
+//     for (let i = 0; i < existingComments.length; i++) {
+//         let commentElement = document.createElement('div');
+//         commentElement.classList.add('comment');
+//         commentElement.textContent = existingComments[i].textContent;
+//         commentsContainer.appendChild(commentElement);
+//     }
+
+//     commentModal.style.display = 'block';
+//     // 댓글을 새로 작성할 때 이전 내용 초기화
+//     document.getElementById('comment-content').value = '';
+// }
+
+// function closeCommentModal() {
+//     commentModal.style.display = 'none';
+// }
+
+router.get('/getPosts',(req,res)=>{
+    try{
+        console.log("getPosts 들어옴");
+        dbPool.query("postList",(err,results)=>{
+            if (err){
+                console.error('게시글 불러오기 중 mysql 오류',err);
+                res.status(500).json({error:'게시글을 불러오는 동안 오류가 발생'});
+
+            }else{
+                res.json(results);
+            }
+        });
+    }catch(error){
+        console.error('게시글 불러오기 중 오류 발생',error);
+        res.status(500).json({error: '게시글을 불러오는 동안 오류 발생'});
     }
+    // const sortedPosts = postsDatabase.slice().reverse();
+    // res.status(200).json(sortedPosts);
+});
 
-    commentModal.style.display = 'block';
-    // 댓글을 새로 작성할 때 이전 내용 초기화
-    document.getElementById('comment-content').value = '';
-}
-
-function closeCommentModal() {
-    commentModal.style.display = 'none';
-}
-
-function savePost() {
-    let postContent = document.getElementById('post-content').value;
-
-    if (postContent.trim() !== '') {
-        let postElement = document.createElement('div');
-        postElement.classList.add('post-box');
-        postElement.onclick = function () {
-            openCommentModal(postElement);
+// 간단한 배열을 데이터베이스로 사용
+// const postsDatabase = [];
+router.post("/savePost",async(req,res)=>{
+    try{
+        const post_id = req.params.post_id;
+        console.log("저장하기 버튼 함수는 들어옴")
+        const postInfo = {
+            date: req.body.postDate,
+            price: req.body.postPrice,
+            content:req.body.postContent
+            
         };
+        // console.log(postInfo)
+        if (postInfo && postInfo.content) {
+            const content = postInfo.content.trim();
+            
+        
+            if (content !== '') {
+                // 게시물 저장 로직
+                // console.log("메세지 입력해봐")
+                // const sql = `INSERT INTO post (date, price, text) VALUES (?, ?, ?)`;
+                console.log('Before query execution');
+                try{
+                    dbPool.query('postInsert', [postInfo.date, postInfo.price, postInfo.content]);
+                    console.log('저장 완료!');
+                    res.status(200).send('게시물이 성공적으로 저장되었습니다.');
+                }  catch (err) {
+                    console.error('게시물 저장 중 MySQL 오류:', err);
+                    res.status(500).json({ error: '게시물을 저장하는 동안 오류가 발생했습니다.' });
+                
+                    // if (err) {
+                    //     console.error('게시물 저장 중 MySQL 오류:', err);
+                    //     res.status(500).json({ error: '게시물을 저장하는 동안 오류가 발생했습니다.' });
+                    //   } else {
+                    //     console.log('게시물이 성공적으로 저장되었습니다.');
+                    //     res.status(200).send('게시물이 성공적으로 저장되었습니다.');
+                    //   }
+                // postsDatabase.push(postInfo);
+                // console.log(postsDatabase);
 
-        let postContentElement = document.createElement('div');
-        postContentElement.classList.add('post-content');
-        postContentElement.textContent = postContent;
-        postElement.appendChild(postContentElement);
+                } 
+                console.log('After query execution');
+            }
+        }
+            }catch (error) {
+        console.error('Error during savepost:', error);
+        // res.status(500).json({ error: 'Internal Server Error' });
+            }
+    });
 
-        let commentsContainer = document.createElement('div');
-        commentsContainer.classList.add('comments-container');
-        postElement.appendChild(commentsContainer);
 
-        postsContainer.appendChild(postElement);
 
-        // 저장 후 창 닫기
-        closePostModal();
-    } else {
-        alert('포스트 내용을 입력하세요.');
-    }
-}
+// function savePost() {
+//     let postInfo = {
+//         postDate: document.getElementById('date').value,
+//         postPrice: document.getElementById('price').value,
+//         postContent: document.getElementById('post-content').value
+//       };
+
+//     if (postInfo.postContent.trim() !== ''|| postInfo.postPrice.trim()!=='') {
+//         // 게시물 댓글 통째로 담기
+//         let postElement = document.createElement('div');
+//         postElement.classList.add('post-box');
+//         postElement.onclick = function () {
+//             openCommentModal(postElement);
+//         };
+//         // 게시물 내용 담기
+//         let postContentElement = document.createElement('div');
+//         postContentElement.classList.add('post-content');
+//         postContentElement.textContent = `Date: ${postInfo.postDate}`;
+//         postContentElement.textContent += `, Price: ${postInfo.postPrice}`;
+//         postContentElement.textContent += `, Content: ${postInfo.postContent}`;
+//         postElement.appendChild(postContentElement);
+
+//         // 댓글 담기
+//         let commentsContainer = document.createElement('div');
+//         commentsContainer.classList.add('comments-container');
+//         postElement.appendChild(commentsContainer);
+
+
+//         // 상단에 게시글 추가
+//         postsContainer.insertBefore(postElement, postsContainer.firstChild);
+
+        
+//         // 저장 후 창 닫기
+//         closePostModal();
+
+//     } else {
+//         alert('가격과 포스트 내용을 입력하세요.');
+//     }
+// }
+// function displayPosts() {
+//     const threadContainer = document.getElementById('thread-container');
+//     threadContainer.innerHTML = ''; // 이전 게시물 내용을 초기화
+
+//     // 시간 역순으로 정렬하여 최신 글이 위로 오도록 함
+//     const sortedPosts = postsDatabase.slice().reverse();
+
+//     sortedPosts.forEach((postInfo, index) => {
+//         const postElement = document.createElement('div');
+//         postElement.classList.add('post-box');
+//         postElement.innerHTML = `
+//             <p><strong>게시일:</strong> ${postInfo.postDate}</p>
+//             <p><strong>가격:</strong> ${postInfo.postPrice}</p>
+//             <p><strong>내용:</strong> ${postInfo.postContent}</p>
+//         `;
+
+//         // 새로운 게시물을 맨 위에 추가
+//         // threadContainer.insertBefore(postElement, threadContainer.firstChild);
+//     });
+// }
 
 function saveComment() {
     let commentContent = document.getElementById('comment-content').value;
@@ -98,3 +208,5 @@ function updateCommentModal() {
         commentsContainer.appendChild(commentElement);
     }
 }
+
+module.exports=router;
--- mysql/sql.js
+++ mysql/sql.js
@@ -1,7 +1,8 @@
 module.exports = {
   userList: `select * from userTable`,
   userInsert: `insert into userTable set ?`,
-
+  postInsert:`INSERT INTO post (date, price, text) VALUES (?, ?, ?)`,
+  postList:`SELECT * FROM post`,
   // 임시로 과제에 사용했던 거 넣어둠
   // 이게 아마 개 많을 것으로 예상
 };
