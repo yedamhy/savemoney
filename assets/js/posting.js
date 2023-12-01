e5b3fbb7647c943e7bf8a972bed748debc14c1ac
Jihye <jihyeeee511@gmail.com>
2023-12-01 15:50:10 +0900

    메인페이지 기능,레이아웃 구현 거의 최종

--- assets/js/posting.js
+++ assets/js/posting.js
@@ -19,36 +19,10 @@ router.get("/",function(req,res,next) {
         }
       });
     });
-    
-// 이거 영주가 처음에 개발했을때 만들어 둔건데 혹시 도움될까봐 지우지않을게! 필요없으면 지워도 됩니당
-// function openCommentModal(postElement) {
-//     currentPost = postElement;
-//     let postContent = postElement.getElementsByClassName('post-content')[0].textContent;
-//     document.getElementById('post-content-container').innerHTML = '<strong>게시글:</strong> ' + postContent;
-
-//     let commentsContainer = document.getElementById('comments-container');
-//     commentsContainer.innerHTML = '';
-
-//     let existingComments = postElement.getElementsByClassName('comment');
-//     for (let i = 0; i < existingComments.length; i++) {
-//         let commentElement = document.createElement('div');
-//         commentElement.classList.add('comment');
-//         commentElement.textContent = existingComments[i].textContent;
-//         commentsContainer.appendChild(commentElement);
-//     }
-
-//     commentModal.style.display = 'block';
-//     // 댓글을 새로 작성할 때 이전 내용 초기화
-//     document.getElementById('comment-content').value = '';
-// }
-
-// function closeCommentModal() {
-//     commentModal.style.display = 'none';
-// }
 
 router.get('/getPosts',(req,res)=>{
     try{
-        console.log("getPosts 들어옴");
+        // console.log("getPosts 들어옴");
         dbPool.query("postList",(err,results)=>{
             if (err){
                 console.error('게시글 불러오기 중 mysql 오류',err);
@@ -70,7 +44,7 @@ router.get('/getPosts',(req,res)=>{
 router.post("/savePost",async(req,res)=>{
     try{
         const post_id = req.params.post_id;
-        console.log("저장하기 버튼 함수는 들어옴")
+        
         const postInfo = {
             date: req.body.postDate,
             price: req.body.postPrice,
@@ -83,12 +57,14 @@ router.post("/savePost",async(req,res)=>{
             
         
             if (content !== '') {
+                const formattedDate = new Date(postInfo.date).toISOString().slice(0, 10);
+                
                 // 게시물 저장 로직
                 // console.log("메세지 입력해봐")
                 // const sql = `INSERT INTO post (date, price, text) VALUES (?, ?, ?)`;
                 console.log('Before query execution');
                 try{
-                    dbPool.query('postInsert', [postInfo.date, postInfo.price, postInfo.content]);
+                    dbPool.query('postInsert', [formattedDate, postInfo.price, postInfo.content]);
                     console.log('저장 완료!');
                     res.status(200).send('게시물이 성공적으로 저장되었습니다.');
                 }  catch (err) {
@@ -117,65 +93,33 @@ router.post("/savePost",async(req,res)=>{
 
 
 
-// function savePost() {
-//     let postInfo = {
-//         postDate: document.getElementById('date').value,
-//         postPrice: document.getElementById('price').value,
-//         postContent: document.getElementById('post-content').value
-//       };
+//아래는 영주가 만들었던 댓글에 대한 코드인데 혹시나 몰라서 일단 남겨둘게!
 
-//     if (postInfo.postContent.trim() !== ''|| postInfo.postPrice.trim()!=='') {
-//         // 게시물 댓글 통째로 담기
-//         let postElement = document.createElement('div');
-//         postElement.classList.add('post-box');
-//         postElement.onclick = function () {
-//             openCommentModal(postElement);
-//         };
-//         // 게시물 내용 담기
-//         let postContentElement = document.createElement('div');
-//         postContentElement.classList.add('post-content');
-//         postContentElement.textContent = `Date: ${postInfo.postDate}`;
-//         postContentElement.textContent += `, Price: ${postInfo.postPrice}`;
-//         postContentElement.textContent += `, Content: ${postInfo.postContent}`;
-//         postElement.appendChild(postContentElement);
+// function openCommentModal(postElement) {
+//     currentPost = postElement;
+//     let postContent = postElement.getElementsByClassName('post-content')[0].textContent;
+//     document.getElementById('post-content-container').innerHTML = '<strong>게시글:</strong> ' + postContent;
 
-//         // 댓글 담기
-//         let commentsContainer = document.createElement('div');
-//         commentsContainer.classList.add('comments-container');
-//         postElement.appendChild(commentsContainer);
+//     let commentsContainer = document.getElementById('comments-container');
+//     commentsContainer.innerHTML = '';
 
-
-//         // 상단에 게시글 추가
-//         postsContainer.insertBefore(postElement, postsContainer.firstChild);
-
-        
-//         // 저장 후 창 닫기
-//         closePostModal();
-
-//     } else {
-//         alert('가격과 포스트 내용을 입력하세요.');
+//     let existingComments = postElement.getElementsByClassName('comment');
+//     for (let i = 0; i < existingComments.length; i++) {
+//         let commentElement = document.createElement('div');
+//         commentElement.classList.add('comment');
+//         commentElement.textContent = existingComments[i].textContent;
+//         commentsContainer.appendChild(commentElement);
 //     }
+
+//     commentModal.style.display = 'block';
+//     // 댓글을 새로 작성할 때 이전 내용 초기화
+//     document.getElementById('comment-content').value = '';
 // }
-// function displayPosts() {
-//     const threadContainer = document.getElementById('thread-container');
-//     threadContainer.innerHTML = ''; // 이전 게시물 내용을 초기화
 
-//     // 시간 역순으로 정렬하여 최신 글이 위로 오도록 함
-//     const sortedPosts = postsDatabase.slice().reverse();
-
-//     sortedPosts.forEach((postInfo, index) => {
-//         const postElement = document.createElement('div');
-//         postElement.classList.add('post-box');
-//         postElement.innerHTML = `
-//             <p><strong>게시일:</strong> ${postInfo.postDate}</p>
-//             <p><strong>가격:</strong> ${postInfo.postPrice}</p>
-//             <p><strong>내용:</strong> ${postInfo.postContent}</p>
-//         `;
-
-//         // 새로운 게시물을 맨 위에 추가
-//         // threadContainer.insertBefore(postElement, threadContainer.firstChild);
-//     });
+// function closeCommentModal() {
+//     commentModal.style.display = 'none';
 // }
+
 
 function saveComment() {
     let commentContent = document.getElementById('comment-content').value;
---
+++ assets/mypost.js
@@ -0,0 +1,156 @@
+// mypost는 게시글 불러오는걸 자기 아이디에 해당되는 것만 뽑아서 불러오기 
+ 
+
+//사용자의 아이디로 작성한 게시글만 불러오기
+// async function fetchAndDisplayPosts() {
+//     try {
+//       // 로그인한 사용자의 아이디 가져오기 (가정)
+//       const userId = getLoggedInUserId(); // 이 함수는 실제 상황에 맞게 수정해야 합니다.
+  
+//       // 서버에서 로그인한 사용자의 아이디로 작성된 게시글 데이터 불러오기
+//       const response = await fetch(`/getPosts?userId=${userId}`);
+//       const data = await response.json();
+//       console.log(data);
+  
+//       // 화면에 게시글을 표시
+//       displayPosts(data.reverse());
+  
+//     } catch (error) {
+//       console.error('게시글 불러오기 중 오류 발생');
+//     }
+//   }
+ //게시글을 불러와 화면에 표시하는 함수
+ async function fetchAndDisplayPosts(){
+    try{
+      //서버에서 기존 게시글 데이터 불러옴
+      const response = await fetch("/getPosts");
+      const data = await response.json();
+      console.log(data);
+      //화면에 게시글을 표시
+      displayPosts(data.reverse());
+
+    }catch (error){
+      console.error('게시글 불러오기 중 오류 발생');
+    }
+  }
+
+  
+  // 게시글을 HTML 문자열로 변환하여 화면에 표시하는 함수
+  function displayPosts(postsData){
+    console.log(postsData);
+    const threadContainer = document.getElementById('thread-container');
+    threadContainer.innerHTML='';
+
+    const limitedPosts = postsData.slice(0, 6);
+    console.log(limitedPosts);
+    limitedPosts.forEach((postInfo,index)=>{
+      const postDate = new Date(postInfo.date);
+      const postElement = document.createElement('div');
+      postElement.classList.add('post-box');
+      postElement.innerHTML=`
+      <p><strong>게시일:</strong> ${postDate.toLocaleDateString()}</p>
+      <p><strong>가격:</strong> ${postInfo.price}</p>
+      <p><strong>내용:</strong> ${postInfo.text}</p>
+      `;
+      //클릭 이벤트 리스너 추가
+      postElement.onclick= function(){
+        openModalWithPost(postInfo);
+      };
+
+      threadContainer.appendChild(postElement);
+    })
+   
+  }
+  //페이지 로드시 자동으로 게시글을 불러와서 표시
+  window.onload = fetchAndDisplayPosts;
+
+
+
+  //게시글을 저장하고 화면을 갱신하는 함수
+  async function savePost(){
+
+    const postDate= document.getElementById('date');
+    const postPrice= document.getElementById('price');
+    const postContent= document.getElementById('post-content');
+
+    if(postDate && postPrice && postContent){
+      const postInfo ={
+        postDate: postDate.value,
+        postPrice: postPrice.value,
+        postContent: postContent.value
+      };
+      try{
+        //서버에 post요청으로 데이터 전송(db저장을 위해)
+        console.log(postInfo);
+        const response = await fetch('/savePost',{
+          
+          method:'POST',
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(postInfo)
+        });
+
+        if (response.ok) {
+          console.log('게시물이 성공적으로 저장되었습니다.');
+          //서버에 저장된 게시글 데이터를 다시 가져와서 화면 갱신
+          closePostModal();
+          await fetchAndDisplayPosts();
+          // closePostModal();
+          // displayPosts();
+        } else {
+            const errorData = await response.json();
+            alert(errorData.error);
+        }
+      } catch (error) {
+        console.error('게시물 저장 중 오류 발생:', error);
+      }
+    }else {
+        console.error('요소를 찾을 수 없거나 값이 비어 있습니다.');
+    }
+  }
+
+  //게시글 입력 창 열기
+  function openPostModal() {
+      let modal = document.getElementById('post-modal');
+      modal.style.display = 'block';
+      // 새로운 글을 작성할 때 이전 내용 초기화
+      document.getElementById('post-content').value = '';
+  }
+
+  //게시글 입력 창 끄기
+  function closePostModal() {
+      // 모달 창의 HTML 엘리먼트를 가져옵니다.
+      let modal = document.getElementById('post-modal');
+
+      // 모달 창을 숨기도록 스타일을 변경합니다.
+      modal.style.display = 'none';
+  }
+
+
+
+  
+
+  // 게시글 창 열기 함수
+  function openModalWithPost(postInfo) {
+    const modal = document.getElementById('myModal');
+    const modalContent = document.getElementById('modalContent');
+      console.log('클릭한 게시글의 정보:', postInfo);
+      modalContent.innerHTML = `
+    <p><strong>게시일:</strong> ${postInfo.date}</p>
+    <p><strong>가격:</strong> ${postInfo.price}</p>
+    <p><strong>내용:</strong> ${postInfo.text}</p>
+    `;
+    // 모달을 보이게 합니다.
+    modal.style.display = 'block';
+
+    // 추가: 모달 외부를 클릭하면 모달이 닫히도록 설정
+    window.onclick = function(event) {
+        const modal = document.getElementById('myModal'); // 모달의 ID를 'myModal'로 수정
+        if (event.target == modal) {
+          modal.style.display = 'none';
+        }
+      };
+  }
+  
+  
