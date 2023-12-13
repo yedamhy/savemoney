35788080f0deacc0d0b1131172e00b8aafb421da
oz115 <yjhg1105@naver.com>
2023-12-13 11:52:28 +0900

    tw

--- login/config/kakao.js
+++ login/config/kakao.js
@@ -2,5 +2,5 @@
 module.exports = {
   clientID: "5baab2d43a20bfe012d6a7adafdbd477",
   clientSecret: "3vXooJjwRZeKlqnHuqqcrYuhQ1hjuR3J",
-  redirectUri: "http://localhost:8080/auth/kakao/callback",
+  redirectUri: "http://35.227.159.136:8080/auth/kakao/callback",
 };
