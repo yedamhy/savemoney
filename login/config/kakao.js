e65e19a5fc42ca01e37f97996687824d7fb4d135
oz115 <yjhg1105@naver.com>
2023-12-13 10:58:50 +0900

    경로수정

--- login/config/kakao.js
+++ login/config/kakao.js
@@ -1,6 +1,6 @@
 // 카카오 API 관련 설정 저장
 module.exports = {
-    clientID: '5baab2d43a20bfe012d6a7adafdbd477',
-    clientSecret: '3vXooJjwRZeKlqnHuqqcrYuhQ1hjuR3J',
-    redirectUri: 'http://localhost:8080/auth/kakao/callback'
+  clientID: "5baab2d43a20bfe012d6a7adafdbd477",
+  clientSecret: "3vXooJjwRZeKlqnHuqqcrYuhQ1hjuR3J",
+  redirectUri: "http://35.227.159.136:8080/auth/kakao/callback",
 };
